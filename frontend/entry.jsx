d4c92777042714f01545b34f38a6b01af2873423
Samuel Song <samsongs@umich.edu>
2022-06-20 11:25:17 -0500

    Add root code for test render on browser

---
+++ frontend/entry.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import { createRoot } from 'react-dom/client';
+
+document.addEventListener("DOMContentLoaded", () => {
+    const root = createRoot(document.getElementById("root"));
+    root.render();
+});
