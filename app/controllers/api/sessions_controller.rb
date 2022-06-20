a851f64f480b124e1c7b106fcd7a3097f64ed8e8
Samuel Song <samsongs@umich.edu>
2022-06-20 13:41:50 -0500

    Complete create and destroy actions

--- app/controllers/api/sessions_controller.rb
+++ app/controllers/api/sessions_controller.rb
@@ -1,11 +1,21 @@
 class Api::SessionsController < ApplicationController
 
     def create
-
+        @user = User.find_by_credentials(params[:user][:email], params[:user][:password])
+        if @user
+            signin(@user)
+            render "/api/users/show"
+        else
+            render json: { signin: "Login failed" }, status: 401
+        end
     end
 
     def destroy
-
+        if current_user
+            signout
+        else
+            render json: { signout: "Already signed out" }, status: 404
+        end
     end
     
 end
