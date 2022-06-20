d06aac2a137bce54fcad4c74e203b5dddbbbd01e
Samuel Song <samsongs@umich.edu>
2022-06-20 12:11:32 -0500

    Add skeleton for create action to user controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,7 +1,20 @@
 class Api::UsersController < ApplicationController
 
     def create
-        
+        @user = User.new(user_params)
+
+        if @user.save
+            # sign in the user
+            # render the show view = pass to the client the user data
+        else
+            # render to the client errors about email / password 
+        end
+    end
+
+    private
+
+    def user_params
+        params.require(:user).permit(:email, :password)
     end
     
 end
