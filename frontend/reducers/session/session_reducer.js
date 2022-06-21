434f6c26b600523f85cef78bdbe2503e4ca02a7d
Samuel Song <samsongs@umich.edu>
2022-06-20 19:22:22 -0500

    Add switch case for receive and remove user

--- frontend/reducers/session/session_reducer.js
+++ frontend/reducers/session/session_reducer.js
@@ -1,6 +1,12 @@
+import { RECEIVE_USER, REMOVE_USER } from "../../actions/session_actions.js";
+
 const sessionReducer = (prevState={id: null}, action) => {
     Object.freeze(prevState);
     switch(action.type) {
+        case RECEIVE_USER:
+            return { id: action.user.id };
+        case REMOVE_USER:
+            return { id: null };
         default:
             return prevState;
     }
