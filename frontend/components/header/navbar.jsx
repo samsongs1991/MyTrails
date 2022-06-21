0b382944f288c1891aca06c9d3a8aaa920273faf
Samuel Song <samsongs@umich.edu>
2022-06-21 15:41:35 -0500

    Add redux container, sessionLinks, profileIcon

--- frontend/components/header/navbar.jsx
+++ frontend/components/header/navbar.jsx
@@ -1,12 +1,40 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import { connect } from "react-redux";
+import { signOut } from "../../actions/session_actions.js";
 
-const NavBar = () => (
-    <nav>
-        <Link to="/trails">Explore</Link>
-        <Link to="/"><img src="logo.png" alt="MyTrails Logo"/></Link>
-        {/* sign in / sign up buttons */}
-    </nav>
-);
+const NavBar = props => {
+    const { users, id, signOut } = props;
 
-export default NavBar;
+    const sessionLinks = (
+        <div>
+            <Link to="/signup">Sign Up</Link>
+            <Link to="/signin">Log In</Link>
+        </div>
+    );
+        
+    const profileIcon = (
+        <div>
+            
+        </div>
+    );
+    
+    return (
+        <nav>
+            <Link to="/trails">Explore</Link>
+            <Link to="/"><img src="logo.png" alt="MyTrails Logo"/></Link>
+            {id === null ? sessionLinks : profileIcon}
+        </nav>
+    );
+};
+
+const mSTP = state => ({
+    users: state.entities.users,
+    id: state.session.id
+});
+
+const mDTP = dispatch => ({
+    signOut: () => dispatch(signOut())
+});
+
+export default connect(mSTP, mDTP)(NavBar);
