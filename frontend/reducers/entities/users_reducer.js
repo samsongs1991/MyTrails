fda53e83a93180c0e08f8307c5f8083d9567df1e
Samuel Song <samsongs@umich.edu>
2022-06-20 19:25:28 -0500

    Add switch cases for receive and remove user

--- frontend/reducers/entities/users_reducer.js
+++ frontend/reducers/entities/users_reducer.js
@@ -1,6 +1,12 @@
+import { RECEIVE_USER, REMOVE_USER } from "../../actions/session_actions.js";
+
 const usersReducer = (prevState={}, action) => {
     Object.freeze(prevState);
     switch(action.type) {
+        case RECEIVE_USER:
+            return { [action.user.id]: action.user };
+        case REMOVE_USER:
+            return {};
         default: 
             return prevState;
     }
