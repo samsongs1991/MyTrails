4617010a137355ab38357b322f8e3a70580d4443
Samuel Song <samsongs@umich.edu>
2022-06-20 19:27:10 -0500

    Add switch case for receive session errors

--- frontend/reducers/errors/session_errors_reducer.js
+++ frontend/reducers/errors/session_errors_reducer.js
@@ -1,6 +1,10 @@
+import { RECEIVE_SESSION_ERRORS } from "../../actions/session_actions.js";
+
 const sessionErrorsReducer = (prevState=[], action) => {
     Object.freeze(prevState);
     switch(action.type) {
+        case RECEIVE_SESSION_ERRORS:
+            return action.errors;
         default:
             return prevState;
     }
