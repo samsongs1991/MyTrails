9ff2b797a0bb1074fefa7e4f1d2a1cdde84ba845
Samuel Song <samsongs@umich.edu>
2022-06-20 19:31:50 -0500

    Add switch cases to clear errors on successful signin and signout

--- frontend/reducers/errors/session_errors_reducer.js
+++ frontend/reducers/errors/session_errors_reducer.js
@@ -1,10 +1,14 @@
-import { RECEIVE_SESSION_ERRORS } from "../../actions/session_actions.js";
+import { RECEIVE_SESSION_ERRORS, RECEIVE_USER, REMOVE_USER } from "../../actions/session_actions.js";
 
 const sessionErrorsReducer = (prevState=[], action) => {
     Object.freeze(prevState);
     switch(action.type) {
         case RECEIVE_SESSION_ERRORS:
             return action.errors;
+        case RECEIVE_USER:
+            return [];
+        case REMOVE_USER:
+            return [];
         default:
             return prevState;
     }
