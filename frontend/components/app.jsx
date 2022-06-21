ed77baad76a94d91513cfffa6479abf5bcb58ec8
Samuel Song <samsongs@umich.edu>
2022-06-20 19:46:23 -0500

    Setup skeleton root and app components with Provider and HashRouter

---
+++ frontend/components/app.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const App = () => (
+    <>
+        APP component
+    </>
+);
+
+export default App;
--- frontend/entry.jsx
+++ frontend/entry.jsx
@@ -1,15 +1,13 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
+import Root from "./components/root.jsx";
 
 document.addEventListener("DOMContentLoaded", () => {
     const root = createRoot(document.getElementById("root"));
-    root.render(<>React is working! - Testing</>);
+    root.render(<Root/>);
 });
 
 // = = = = = = = = = = = =
 // = = = = TESTING = = = =
 // = = = = = = = = = = = =
 
-import store from "./store/store.js";
-
-window.store = store;
