91fa5a6e4980bd9c0814fc3a66e13615a47ba86c
Samuel Song <samsongs@umich.edu>
2022-06-23 18:56:26 -0500

    Add route for home comp with background img

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,13 +2,14 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import Header from "./header/header.jsx";
+import Home from "./home/home.jsx";
 import SignUpForm from "./session_forms/signup_form.jsx";
 import SignInForm from "./session_forms/signin_form.jsx";
 
 const App = () => (
     <>
         <Header/>
-
+        <Route path="/" exact component={Home}/>
         <AuthRoute path="/signup" component={SignUpForm}/>
         <AuthRoute path="/signin" component={SignInForm}/>
     </>
--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,7 +1,11 @@
 import React from "react";
 
 const Home = props => {
-
+    return (
+        <section id="home-page">
+            HOME COMPONENT
+        </section>
+    );
 };
 
 export default Home;
