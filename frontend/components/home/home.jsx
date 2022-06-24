b6e91ca3adc6228ba8307c9a4219a663588a4a6c
Samuel Song <samsongs@umich.edu>
2022-06-24 17:33:23 -0500

    Add greeting to home comp

--- frontend/components/home/home.jsx
+++ frontend/components/home/home.jsx
@@ -1,20 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const Home = props => {
+const Home = ({ user }) => {
     return (
         <section id="home-page">
             HOME COMPONENT
+            {/* img */}
+            <section id="home-image">
+                {user ? <div>Ready to do this, {user.fname}?</div> : null}
+            </section>
+            {/* search bar */}
+            {/* <SearchBar/> */}
+            {/* trail cards */}
+            {/* <Trails/> */}
         </section>
     );
 };
 
 const mSTP = state => ({
-
+    user: state.entities.users[state.session.id]
 });
 
-const mDTP = dispatch => ({
-
-});
-
-export default connect(mSTP, mDTP)(Home);
+export default connect(mSTP)(Home);
