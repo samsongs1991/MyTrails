ffb7c13b7b3d17949687508790aeb6dd9a1de9dc
Samuel Song <samsongs@umich.edu>
2022-06-24 14:08:10 -0500

    Create skeleton for adding trail seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,27 +9,29 @@
 # Demo user
 User.create(email: "demo@gmail.com", password: 123456, fname: "Sam", lname: "Song", about_me: "I am the creator of this AllTrails clone site. I actually do love hiking and being outside with nature. My favorite hike is Bandera Mountain near Seattle, WA.", city: "Dallas", state: "TX")
 
-# trails = [
-#     [name, lat, lng, location, city, state, country, difficulty, length, gain, category], 
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     [],
-#     []
-# ]
-# Trail.create()
+trails = [
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+    { name: "", lat: "", lng: "", location: "", city: "", state: "", country: "", difficulty: "", length: "", gain: "", category: "" }, 
+]
+
+trails.each do |trail|
+    Trail.create(trail)
+end
