1c172e122115f8f8ebe66f8d610d6819f9c26a7d
Samuel Song <samsongs@umich.edu>
2022-06-24 13:59:49 -0500

    Add demo user to seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,31 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# Demo user
+User.create(email: "demo@gmail.com", password: 123456, fname: "Sam", lname: "Song", about_me: "I am the creator of this AllTrails clone site. I actually do love hiking and being outside with nature. My favorite hike is Bandera Mountain near Seattle, WA.", city: "Dallas", state: "TX")
+
+# trails = [
+#     [name, lat, lng, location, city, state, country, difficulty, length, gain, category], 
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     [],
+#     []
+# ]
+# Trail.create()
