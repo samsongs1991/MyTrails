3d7fdfd57d63ad77e38d1e02120e0f1406c025d9
Samuel Song <samsongs@umich.edu>
2022-06-24 22:44:42 -0500

    Fix typo in reducer and add options to dropdown

--- frontend/reducers/entities/trails_reducer.js
+++ frontend/reducers/entities/trails_reducer.js
@@ -1,6 +1,6 @@
 import { RECEIVE_ALL_TRAILS } from "../../actions/trail_actions.js";
 
-const usersReducer = (prevState={}, action) => {
+const trailsReducer = (prevState={}, action) => {
     Object.freeze(prevState);
     switch(action.type) {
         case RECEIVE_ALL_TRAILS:
