cbc446d312efdffbcb4170055ec60c6898386c14
Samuel Song <samsongs@umich.edu>
2022-06-24 22:54:06 -0500

    Add comments for next steps and add session page images

--- frontend/components/home/search_bar.jsx
+++ frontend/components/home/search_bar.jsx
@@ -1,14 +1,24 @@
 import React from "react";
 import { connect } from "react-redux";
 
+
+// Since datalist options have little flexibility for css, convert the dropdown to be a modal
+// Then style the modal options
+
+
+
 const SearchBar = ({ trails }) => {        
     return (
         <form id="search-bar">
             <img src="/home_images/search_icon.png" alt="Search Icon"/>
             <input list="dropdown" placeholder="Search by city, park, or trail name"/>
+
+            {/* ========================================= */}
             <datalist id="dropdown">
                 {Object.values(trails).map((trail, i) => <option key={i} value={trail.name}/>)}
             </datalist>
+            {/* ========================================= */}
+            
             <img src="/home_images/arrow.png" alt="Submit"/>
         </form>
     );
