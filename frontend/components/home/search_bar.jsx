7d2c6a1f3c77d00fe4b23b4201433fd1a417c47b
Samuel Song <samsongs@umich.edu>
2022-06-24 20:06:19 -0500

    Change input to dropdown

--- frontend/components/home/search_bar.jsx
+++ frontend/components/home/search_bar.jsx
@@ -4,7 +4,10 @@ const SearchBar = props => {
     return (
         <form id="search-bar">
             <img src="/home_images/search_icon.png" alt="Search Icon"/>
-            <input placeholder="Search by city, park, or trail name"/>
+            <input list="dropdown" placeholder="Search by city, park, or trail name"/>
+            <datalist id="dropdown">
+                <option value="testing"/>
+            </datalist>
             <img src="/home_images/arrow.png" alt="Submit"/>
         </form>
     );
