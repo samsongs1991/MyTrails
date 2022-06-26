f55721c06cb946a2ec30dbff688fc0cab8f8aba2
Samuel Song <samsongs@umich.edu>
2022-06-26 13:30:06 -0500

    Convert li to Link comps

--- frontend/components/home/search_item.jsx
+++ frontend/components/home/search_item.jsx
@@ -1,21 +1,14 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
-const SearchItem = ({ trail }) => {
-
-    const handleItem = e => {
-        e.stopPropagation();
-        // link to item show page
-    };
-    
-    return (
-        <li onClick={handleItem}>
-            <img src="/home_images/location_icon.png" alt="Location Icon"/>
-            <article>
-                <div>{trail.name}</div>
-                <div>{trail.location}, {trail.city}, {trail.state}</div>
-            </article>
-        </li>
-    );
-};
+const SearchItem = ({ trail }) => (
+    <Link to={`/trails/${trail.id}`} onClick={e => e.stopPropagation()}>
+        <img src="/home_images/location_icon.png" alt="Location Icon"/>
+        <article>
+            <div>{trail.name}</div>
+            <div>{trail.location}, {trail.city}, {trail.state}</div>
+        </article>
+    </Link>
+);
 
 export default SearchItem;
