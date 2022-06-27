d9f4f7277f8338330a623dbd82aa3c46f8becb9c
Samuel Song <samsongs@umich.edu>
2022-06-26 23:13:00 -0500

    Add css to card comp

--- frontend/components/home/card.jsx
+++ frontend/components/home/card.jsx
@@ -3,15 +3,13 @@ import { Link } from "react-router-dom";
 
 const Card = ({ trail }) => {
     return (
-        <article>
-            <Link to={`/trails/${trail.id}`}>
-                <img src="" alt={`Image of ${trail.location}`}/>
-                <div>{trail.difficulty} - <img src="" alt="Star"/>5.0</div>
-                <div>{trail.location}</div>
-                <div>{trail.city}, {trail.state}</div>
-                <div>Length: {trail.length} mi - Est. 100min</div>
-            </Link>
-        </article>
+        <Link to={`/trails/${trail.id}`}>
+            <img src="https://www.adorama.com/alc/wp-content/uploads/2018/11/shutterstock_1200681070.jpg" alt={`Image of ${trail.location}`}/>
+            <div><span className={trail.difficulty}>{trail.difficulty}</span> • <img src="/home_images/star_icon.png" alt="Star"/> 5.0</div>
+            <div>{trail.location}</div>
+            <div>{trail.city}, {trail.state}</div>
+            <div>Length: {trail.length} mi • Est. 100min</div>
+        </Link>
     );
 };
 
