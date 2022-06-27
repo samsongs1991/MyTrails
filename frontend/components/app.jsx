6bf05b49144d46729fd309d4a8136a0a04daf2bc
Samuel Song <samsongs@umich.edu>
2022-06-27 14:30:22 -0500

    Add basic footer comp

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Route } from "react-router-dom";
 import { AuthRoute, ProtectedRoute } from "../util/route_util";
 import Header from "./header/header.jsx";
+import Footer from "./footer/footer.jsx";
 import Home from "./home/home.jsx";
 import SignUpForm from "./session_forms/signup_form.jsx";
 import SignInForm from "./session_forms/signin_form.jsx";
@@ -12,6 +13,7 @@ const App = () => (
         <Route path="/" exact component={Home}/>
         <AuthRoute path="/signup" component={SignUpForm}/>
         <AuthRoute path="/signin" component={SignInForm}/>
+        <Footer/>
     </>
 );
 
---
+++ frontend/components/footer/footer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Footer = props => {
+    return (
+        <footer>
+            <div>Connect with the developer</div>
+            <img src="" alt="Photo of the website developer"/>
+            <ul>
+                <img src="" alt="GitHub Icon"/>
+                <img src="" alt="LinkedIn Icon"/>
+                <img src="" alt="AngelList Icon"/>
+                <img src="" alt="Portfolio Icon"/>
+            </ul>
+        </footer>
+    );
+};
+
+export default Footer;
