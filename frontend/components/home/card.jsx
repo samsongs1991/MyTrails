682de97f3a308825740738a90e0ac23b2d77bd0b
Samuel Song <samsongs@umich.edu>
2022-06-27 11:22:10 -0500

    Add time est fn

--- frontend/components/home/card.jsx
+++ frontend/components/home/card.jsx
@@ -2,13 +2,21 @@ import React from "react";
 import { Link } from "react-router-dom";
 
 const Card = ({ trail }) => {
+
+    const calcEst = () => {
+        const mi = trail.length;
+        const mph = 3;
+        const time = Math.ceil((mi / mph) * 60);
+        return time;
+    };
+    
     return (
         <Link to={`/trails/${trail.id}`}>
             <img src="https://www.adorama.com/alc/wp-content/uploads/2018/11/shutterstock_1200681070.jpg" alt={`Image of ${trail.location}`}/>
             <div><span className={trail.difficulty}>{trail.difficulty}</span> • <img src="/home_images/star_icon.png" alt="Star"/> 5.0</div>
             <div>{trail.name}</div>
             <div>{trail.city}, {trail.state}</div>
-            <div>Length: {trail.length} mi • Est. 100min</div>
+            <div>Length: {trail.length} mi • Est. {calcEst()}min</div>
         </Link>
     );
 };
