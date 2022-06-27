d1f97ec535b20a2fd4cb29198ecbabb76db7c8ec
Samuel Song <samsongs@umich.edu>
2022-06-27 15:06:09 -0500

    Add photo of me

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -4,12 +4,12 @@ const Footer = props => {
     return (
         <footer>
             <div>Connect with the developer</div>
-            <img src="" alt="Photo of the website developer"/>
+            <img src="/footer_images/me.jpg" alt="Photo of the website developer"/>
             <ul>
-                <img src="" alt="GitHub Icon"/>
-                <img src="" alt="LinkedIn Icon"/>
-                <img src="" alt="AngelList Icon"/>
-                <img src="" alt="Portfolio Icon"/>
+                <img src="/footer_images/github.png" alt="GitHub Icon"/>
+                <img src="/footer_images/linkedin.png" alt="LinkedIn Icon"/>
+                <img src="/footer_images/angellist.png" alt="AngelList Icon"/>
+                <img src="/footer_images/portfolio.png" alt="Portfolio Icon"/>
             </ul>
         </footer>
     );
