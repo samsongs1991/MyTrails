611355c8b8d954cb3fc08461806052cb54c2beba
Samuel Song <samsongs@umich.edu>
2022-06-27 21:37:37 -0500

    Add fetchTrail thunk action creator

--- frontend/actions/trail_actions.js
+++ frontend/actions/trail_actions.js
@@ -6,9 +6,20 @@ export const fetchAllTrails = () => dispatch => {
         .then(trails => dispatch(receiveAllTrails(trails)));
 };
 
+export const fetchTrail = trailId => dispatch => {
+    Utils.fetchTrail(trailId)
+        .then(trail => dispatch(receiveTrail(trail)));
+};
+
 // Action creators
 export const RECEIVE_ALL_TRAILS = "RECEIVE_ALL_TRAILS";
 const receiveAllTrails = trails => ({
     type: RECEIVE_ALL_TRAILS, 
     trails
+});
+
+export const RECEIVE_TRAIL = "RECEIVE_TRAIL";
+const receiveTrail = trail => ({
+    type: RECEIVE_TRAIL, 
+    trail
 });
