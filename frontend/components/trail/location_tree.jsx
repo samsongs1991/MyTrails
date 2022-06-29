0e061683c924016d59141747404cc47e2e83393d
Samuel Song <samsongs@umich.edu>
2022-06-28 22:22:08 -0500

    Complete locationTree comp

--- frontend/components/trail/location_tree.jsx
+++ frontend/components/trail/location_tree.jsx
@@ -1,19 +1,26 @@
 import React from "react";
+import SearchBar from "../home/search_bar.jsx";
 
 const LocationTree = ({ trail }) => {
 
-    const locations = [
-        trail.country, 
-        trail.state, 
-        trail.city, 
-        trail.location,
-        trail.name
-    ]; 
+    const locationString = () => {
+        if(trail) {
+            const arr = [
+                trail.country, 
+                trail.state, 
+                trail.city, 
+                trail.location,
+                trail.name
+            ];
+
+            return arr.join(" > ");
+        }   
+    };
     
     return trail ? (
         <article>
-            <div>{locations.join(" > ")}</div>
-            <div>search bar</div>
+            <div>{locationString()}</div>
+            <SearchBar/>
         </article>
     ) : null;
 };
