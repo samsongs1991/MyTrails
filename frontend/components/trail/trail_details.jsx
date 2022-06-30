e531935c23f695d3b77d3eccdc6dad49aee2ffdf
Samuel Song <samsongs@umich.edu>
2022-06-30 12:41:06 -0500

    Add commented structure to trail detail comp

--- frontend/components/trail/trail_details.jsx
+++ frontend/components/trail/trail_details.jsx
@@ -4,6 +4,10 @@ const TrailDetails = props => {
     return (
         <section id="trail-details">
             TRAIL DETAILS COMP
+            {/* details */}
+            {/* mini map */}
+            {/* weather widget */}
+            {/* reviews */}
         </section>
     );
 };
