fea9f26057a2e7da80597c8e8a653e94a31249aa
Samuel Song <samsongs@umich.edu>
2022-06-30 14:58:19 -0500

    Fix bug - modal behind trail img

--- frontend/components/trail/details.jsx
+++ frontend/components/trail/details.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Details = ({ trail }) => (
-    <section>
+    <section id="details">
         <div>
             <p>Length</p>
             <p>{trail.length} mi</p>
