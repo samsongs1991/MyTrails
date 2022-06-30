fe77e16f01f04f53888711312c31174d4b9b896c
Samuel Song <samsongs@umich.edu>
2022-06-29 19:05:22 -0500

    Start css for positioning of trail img comp

--- frontend/components/trail/trail_img.jsx
+++ frontend/components/trail/trail_img.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const TrailImg = props => {
     return (
-        <article>
+        <article id="trail-img">
             <div>
                 <p>trail name</p>
                 <p>difficulty + star rating</p>
@@ -10,6 +10,7 @@ const TrailImg = props => {
             <div>
                 <img src="" alt="Add to list icon"/>
             </div>
+            <img src="https://www.adorama.com/alc/wp-content/uploads/2018/11/shutterstock_1200681070.jpg" alt="Image of trail"/>
         </article>
     );
 };
