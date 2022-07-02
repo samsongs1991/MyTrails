c5552e0a7067883c00102f342739cc583f88cb96
Samuel Song <samsongs@umich.edu>
2022-07-02 11:06:44 -0500

    Add skeleton map comp

---
+++ frontend/components/trail/map.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Map = ({ trail }) => {
+    return (
+        <section id="map">
+            MAP COMPONENT
+        </section>
+    );
+};
+
+export default Map;
--- frontend/components/trail/trail_details.jsx
+++ frontend/components/trail/trail_details.jsx
@@ -1,16 +1,13 @@
 import React from "react";
 import Details from "./details.jsx";
+import Map from "./map.jsx";
 import WeatherWidget from "./weather_widget.jsx";
 
 const TrailDetails = ({ trail }) => {
     return (
         <section id="trail-details">
             <Details trail={trail}/>
-
-            <section id="map">
-                MINI MAP
-            </section>
-            
+            <Map trail={trail}/>
             <WeatherWidget trail={trail}/>
 
             <section>
