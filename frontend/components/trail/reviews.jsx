e4f5ef80cb409a789836d7000f8bfdbea13fc2a1
Samuel Song <samsongs@umich.edu>
2022-07-02 16:36:46 -0500

    Add basic outline for reviews comp

--- frontend/components/trail/reviews.jsx
+++ frontend/components/trail/reviews.jsx
@@ -7,8 +7,21 @@ const Reviews = ({ trail }) => {
                 <h2>Reviews</h2>
             </section>
             <section>
-                {/* Avg rating + Write review button */}
+                <article>
+                    <div>
+                        <p>5.0</p>
+                        <div>
+                            star visual comp
+                        </div>
+                        <p># Reviews</p>
+                    </div>
+                    <div>
+                        <button>Write review</button>
+                    </div>
+                </article>
                 {/* List of all reviews */}
+                {/* Iterate through reviews and
+                render a review comp */}
             </section>
         </section>
     );
