7fd4de2f06bb545913cccb2e0012b7e1a98144ea
Samuel Song <samsongs@umich.edu>
2022-07-02 12:00:30 -0500

    Add google map rendered to page

--- frontend/components/trail/map.jsx
+++ frontend/components/trail/map.jsx
@@ -1,9 +1,25 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const Map = ({ trail }) => {
+
+    const [map, setMap] = useState(null);
+    const [mapNode, setMapNode] = useState(null);
+
+    useEffect(() => {
+        const mapOptions = {
+            center: { lat: trail.lat, lng: trail.lng }, 
+            zoom: 13
+        };
+        if(mapNode) {
+            setMap(new google.maps.Map(mapNode, mapOptions));
+        }
+    }, [mapNode]);
+    
     return (
-        <section id="map">
-            MAP COMPONENT
+        <section 
+            id="map-container"
+            ref={map => setMapNode(map)}
+        >
         </section>
     );
 };
