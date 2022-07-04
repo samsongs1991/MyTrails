deaf50c2894bbae7b75d12514ffd1e98d38e0d66
Samuel Song <samsongs@umich.edu>
2022-07-04 13:27:14 -0500

    Add css for review form

--- frontend/components/trail/review_form.jsx
+++ frontend/components/trail/review_form.jsx
@@ -11,23 +11,27 @@ const ReviewForm = ({ trail, setShowModal }) => {
     };
     
     return (
-        <section >
+        <section id="review-form">
             <form onSubmit={handleSubmit}>
-                <img  src="" alt="Close"/>
-                <h3>{trail.name}</h3>
-                <div>
-                    <img id="1" src="/trail_images/star_filled.png" alt="star"/>
-                    <img id="2" src="/trail_images/star_filled.png" alt="star"/>
-                    <img id="3" src="/trail_images/star_filled.png" alt="star"/>
-                    <img id="4" src="/trail_images/star_filled.png" alt="star"/>
-                    <img id="5" src="/trail_images/star_filled.png" alt="star"/>
-                </div>
-                <textarea 
-                    placeholder="Share your thoughts about the trail so others know what to expect."
-                />
-                <div>
-                    <button>Submit</button>
-                </div>
+                <section>
+                    <img onClick={() => setShowModal(false)} src="/trail_images/x_icon.png" alt="Close"/>
+                    <h3>{trail.name}</h3>
+                    <div>
+                        <img id="1" src="/trail_images/star_filled.png" alt="star"/>
+                        <img id="2" src="/trail_images/star_filled.png" alt="star"/>
+                        <img id="3" src="/trail_images/star_filled.png" alt="star"/>
+                        <img id="4" src="/trail_images/star_filled.png" alt="star"/>
+                        <img id="5" src="/trail_images/star_filled.png" alt="star"/>
+                    </div>
+                    <textarea 
+                        placeholder="Share your thoughts about the trail so others know what to expect."
+                    />
+                </section>
+                <section>
+                    <div>
+                        <button>Submit</button>
+                    </div>
+                </section>
             </form>
         </section>
     );
