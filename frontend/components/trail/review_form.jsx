057e6d7cd6667b00c23b6d42792ad49033820e2c
Samuel Song <samsongs@umich.edu>
2022-07-04 16:22:16 -0500

    Add effects for star rating hover

--- frontend/components/trail/review_form.jsx
+++ frontend/components/trail/review_form.jsx
@@ -5,9 +5,43 @@ const ReviewForm = ({ trail, setShowModal }) => {
     const [rating, setRating] = useState(0);
     const [input, setInput] = useState("");
 
+    const handleMouseEnter = e => {
+        const stars = e.target.parentElement.children;
+        for(let i = 0; i < stars.length; i++) {
+            const img = stars[i];
+            if(Number(img.id) <= Number(e.target.id)) {
+                img.classList.add("active");
+            } else {
+                break;
+            }
+        }
+    };
+
+    const handleMouseLeave = e => {
+        const stars = e.target.parentElement.children;
+        for(let i = 0; i < stars.length; i++) {
+            stars[i].classList.remove("active");
+        }
+    };
+    
     const handleRating = e => {
         const id = Number(e.target.id);
-        id === rating ? setRating(0) : setRating(id);
+        const stars = e.target.parentElement.children;
+        if(id === rating) {
+            setRating(0);
+            for(let i = 0; i < stars.length; i++) {
+                stars[i].classList.remove("selected");
+            }
+        } else {
+            setRating(id);
+            for(let i = 0; i < stars.length; i++) {
+                const img = stars[i];
+                img.classList.remove("selected");
+                if(Number(img.id) <= id) {
+                    img.classList.add("selected");
+                }
+            }
+        }        
     };
     
     const handleSubmit = e => {
@@ -17,14 +51,22 @@ const ReviewForm = ({ trail, setShowModal }) => {
     
     return (
         <section id="review-form">
-            {console.log("rating", rating)}
-            {console.log("input", input)}
             <form onSubmit={handleSubmit}>
                 <section>
                     <img onClick={() => setShowModal(false)} src="/trail_images/x_icon.png" alt="Close"/>
                     <h3>{trail.name}</h3>
                     <div>
-                        {[1, 2, 3, 4, 5].map(num => <img onClick={handleRating} id={num} src="/trail_images/star_filled.png" alt="star"/>)}
+                        {[1, 2, 3, 4, 5].map(num => 
+                            <img 
+                                onMouseEnter={handleMouseEnter}
+                                onMouseLeave={handleMouseLeave}
+                                onClick={handleRating} 
+                                id={num} 
+                                key={num}
+                                src="/trail_images/star_filled.png" 
+                                alt="star"
+                            />
+                        )}
                     </div>
                     <textarea 
                         onChange={e => setInput(e.target.value)}
