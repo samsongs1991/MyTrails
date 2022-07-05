589a1b162a6e57d3b74ac17dbc470c0c57ac4dd6
Samuel Song <samsongs@umich.edu>
2022-07-05 12:16:33 -0500

    Complete reviews list comp and create review item comp

---
+++ frontend/components/trail/review_item.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ReviewsItem = ({ review }) => {
+    return (
+        <li>
+            REVIEW ITEM COMP
+        </li>
+    );
+};
+
+export default ReviewsItem;
--- frontend/components/trail/reviews_list.jsx
+++ frontend/components/trail/reviews_list.jsx
@@ -1,9 +1,21 @@
 import React from "react";
+import ReviewItem from "./review_item.jsx";
 
 const ReviewsList = ({ reviews }) => {
+
+    const getLatestReviews = num => {
+        const arr = [];
+        for(let id in reviews) {
+            const review = reviews[id];
+            arr.push(new Date(review.created_at));
+        }
+        arr.sort((a, b) => b - a);
+        return arr.slice(0, num);
+    };
+    
     return (
         <ol>
-            REVIEWS LIST COMPONENT
+            {getLatestReviews(12).map((review, i) => <ReviewItem key={i} review={review}/>)}
         </ol>
     );
 };
