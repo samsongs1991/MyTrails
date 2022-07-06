be0e02fe2ea7678c4717b95d0ed566125c1cbf34
Samuel Song <samsongs@umich.edu>
2022-07-06 11:48:17 -0500

    Edit card comp avg rating

--- frontend/components/home/card.jsx
+++ frontend/components/home/card.jsx
@@ -19,7 +19,7 @@ const Card = ({ trail }) => {
     return (
         <Link to={`/trails/${trail.id}`}>
             <img src="https://www.adorama.com/alc/wp-content/uploads/2018/11/shutterstock_1200681070.jpg" alt={`Image of ${trail.location}`}/>
-            <div><span className={trail.difficulty}>{trail.difficulty}</span> • <img src="/home_images/star_icon.png" alt="Star"/> 5.0</div>
+            <div><span className={trail.difficulty}>{trail.difficulty}</span> • <img src="/home_images/star_icon.png" alt="Star"/> {trail.avg_rating}</div>
             <div>{trail.name}</div>
             <div>{trail.city}, {trail.state}</div>
             <div>Length: {trail.length} mi • Est. {calcTime()}</div>
