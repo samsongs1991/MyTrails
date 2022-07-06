3903d9da5d087b3923bd32bf5a57c893cdf460c7
Samuel Song <samsongs@umich.edu>
2022-07-06 12:13:20 -0500

    Edit trails shown on home page

--- frontend/components/home/trail_cards.jsx
+++ frontend/components/home/trail_cards.jsx
@@ -3,15 +3,26 @@ import { connect } from "react-redux";
 import Card from "./card.jsx";
 
 const TrailCards = ({ trails }) => {
+
+    const getTopTrails = () => {
+        const arr = Object.values(trails);
+        arr.sort((a, b) => {
+            return b.avg_rating - a.avg_rating;
+        });
+        return arr;
+    }
+
+    const topTrails = getTopTrails();
+    
     return (
         <section id="trail-cards-container">
             <section id="trail-cards">
                 <h3>Favorite trails near <span>Seattle</span></h3>
                 <section className="cards">
-                    {Object.values(trails).slice(0,4).map((trail, i) => <Card key={i} trail={trail}/>)}
+                    {topTrails.slice(0,4).map((trail, i) => <Card key={i} trail={trail}/>)}
                 </section>
                 <section className="cards">
-                    {Object.values(trails).slice(4,8).map((trail, i) => <Card key={i} trail={trail}/>)}
+                    {topTrails.slice(4,8).map((trail, i) => <Card key={i} trail={trail}/>)}
                 </section>
             </section>
         </section>
