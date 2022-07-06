0578e9f4f144aeb2f504b261c03fa32679d61a63
Samuel Song <samsongs@umich.edu>
2022-07-06 15:36:00 -0500

    Add filter option modal

--- frontend/components/explore/filter_option.jsx
+++ frontend/components/explore/filter_option.jsx
@@ -1,9 +1,29 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 const FilterOption = ({ type, input, setInput }) => {
+
+    const [showModal, setShowModal] = useState(false);
+        
+    const modal = <div>
+        MODAL
+    </div>
+
+    const hideModal = e => {
+        const btn = document.getElementById(type);
+        if(e.target !== btn) {
+            setShowModal(false);
+        }
+    };
+
+    useEffect(() => {
+        const explorePage = document.getElementById("explore-page");
+        explorePage.addEventListener("click", hideModal);
+    }, []);
+    
     return (
         <div>
-            <button>{type}</button>
+            <button id={type} onClick={() => setShowModal(!showModal)}>{type}</button>
+            {showModal ? modal : null}
         </div>
     );
 };
