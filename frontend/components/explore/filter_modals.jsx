99eea6bb9e58a5214f434bc6cb8b7515ed8bddfc
Samuel Song <samsongs@umich.edu>
2022-07-06 22:12:00 -0500

    Add css to filter form - difficulty

--- frontend/components/explore/filter_modals.jsx
+++ frontend/components/explore/filter_modals.jsx
@@ -1,8 +1,26 @@
 import React from "react";
 
-export const difficulty = <form>difficulty</form>;
+export const difficulty = <form id="form-difficulty">
+    <label>
+        <input value="easy" type="checkbox"/>
+        Easy
+    </label>
+    <label>
+        <input value="moderate" type="checkbox"/>
+        Moderate
+    </label>
+    <label>
+        <input value="hard" type="checkbox"/>
+        Hard
+    </label>
+</form>;
+
 export const length = <form>length</form>;
+
 export const gain = <form>gain</form>;
+
 export const time = <form>time</form>;
+
 export const category = <form>category</form>;
+
 export const rating = <form>rating</form>;
