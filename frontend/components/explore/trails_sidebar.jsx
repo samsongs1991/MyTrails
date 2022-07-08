3adb080268c7364b93c92d942d0a75f4d7ab098c
Samuel Song <samsongs@umich.edu>
2022-07-08 00:23:59 -0500

    Add filtered trails to trails sidebar

--- frontend/components/explore/trails_sidebar.jsx
+++ frontend/components/explore/trails_sidebar.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import Card from "../home/card.jsx";
+import { filter } from "../../util/filter_util.js";
 
-const TrailsSidebar = ({ trails }) => {        
+const TrailsSidebar = ({ trails, options }) => {        
     return (
         <section id="trails-sidebar">
-            {Object.values(trails).map((trail, i) => <Card key={i} trail={trail}/>)}
+            {filter(trails, options).map((trail, i) => <Card key={i} trail={trail}/>)}
         </section>
     );
 };
