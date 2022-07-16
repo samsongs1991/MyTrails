3289eab00a277e9b63b7fc1bae36e20087efed4b
Samuel Song <samsongs@umich.edu>
2022-07-15 23:24:52 -0500

    Change img src to aws img

--- frontend/components/trail/trail_img.jsx
+++ frontend/components/trail/trail_img.jsx
@@ -3,13 +3,13 @@ import React from "react";
 const TrailImg = ({ trail }) => {
     return (
         <section id="trail-img">
-            <img src="https://www.adorama.com/alc/wp-content/uploads/2018/11/shutterstock_1200681070.jpg" alt="Image of trail"/>
+            <img src={trail.trail_img} alt="Image of trail"/>
             <div>
                 <div>
                     <p>{trail.name}</p>
                     <p>
                         <span className={trail.difficulty}>{trail.difficulty}</span>
-                        <img src="/trail_images/star_filled.png" alt="star"/>    
+                        <img src="/trail_images/star_filled.png" alt="star"/>
                         {trail.avg_rating}
                     </p>
                 </div>
