06dccde13da26571fb4ba64fd58d6a549dcdb5ea
Samuel Song <samsongs@umich.edu>
2022-07-17 14:09:38 -0500

    Add css to profile header

--- frontend/components/profile/profile.jsx
+++ frontend/components/profile/profile.jsx
@@ -7,23 +7,25 @@ const Profile = ({ users, id }) => {
 
     return (
         <section id="profile-page">
-            <section>
-                <h2>Profile</h2>
-                <Link to={`/users/${user.id}/edit`}>Edit Profile</Link>
-            </section>
-            <section>
-                <div>
-                    <img src={user.profile_img} alt="profile picture"/>
-                    <p>Member Since</p>
-                    <p>{user.created_at}</p>
-                </div>
-                <div>
-                    <h3>{user.fname} {user.lname}</h3>
-                    <p>{user.city}, {user.state}</p>
-                    <p>{user.email}</p>
-                    <p>{user.about_me}</p>
-                </div>
-            </section>
+            <div>
+                <section>
+                    <h2>Profile</h2>
+                    <Link to={`/users/${user.id}/edit`}>Edit Profile</Link>
+                </section>
+                <section>
+                    <div>
+                        <img src={user.profile_img} alt="profile picture"/>
+                        <p>Member Since</p>
+                        <p>{user.created_at}</p>
+                    </div>
+                    <div>
+                        <h3>{user.fname} {user.lname}</h3>
+                        <p>{user.city}, {user.state}</p>
+                        <p>{user.email}</p>
+                        <p>{user.about_me}</p>
+                    </div>
+                </section>
+            </div>
         </section>
     );
 };
