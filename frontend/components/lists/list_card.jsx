8f2b6414ac57e86d5bf92b14c82d1bff06a7d43a
Samuel Song <samsongs@umich.edu>
2022-07-19 15:24:29 -0500

    Add comments for lists comp

--- frontend/components/lists/list_card.jsx
+++ frontend/components/lists/list_card.jsx
@@ -3,8 +3,9 @@ import React from "react";
 const ListCard = ({ list }) => {
     return (
         <article id="list-card">
-            <img src={list.img_url} alt="Photo for list"/>
-            <div>{list.name}</div>
+            {/* <img src={list.img_url} alt="Photo for list"/>
+            <div>{list.name}</div> */}
+            LIST CARD COMP
         </article>
     );
 };
--- frontend/components/lists/lists.jsx
+++ frontend/components/lists/lists.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import ListCard from "./list_card.jsx";
 
 const Lists = ({ match }) => {
     const userId = match.params.userId;
@@ -12,7 +13,10 @@ const Lists = ({ match }) => {
                     <Link to={`/users/${userId}/lists/edit`}>Edit Lists</Link>
                 </section>
                 <section>
-
+                    <ListCard/>
+                    {/* get lists from state */}
+                    {/* iterate through them as array and return array of list_card comps */}
+                    {/* display lists as photos with list title overlayed on photo */}
                 </section>
             </div>
         </section>
