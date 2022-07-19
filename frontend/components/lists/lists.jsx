e2bb860807428b23196c39d974e9436a67fe2c70
Samuel Song <samsongs@umich.edu>
2022-07-19 18:53:17 -0500

    Add fetchLists to lists comp

--- frontend/components/lists/lists.jsx
+++ frontend/components/lists/lists.jsx
@@ -1,28 +1,26 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import ListCard from "./list_card.jsx";
-import { createList, fetchLists, deleteList } from "../../actions/list_actions.js";
+import { createList, fetchLists } from "../../actions/list_actions.js";
 
 const Lists = ({ match, lists, createList, fetchLists }) => {
     console.log("LISTS COMP props - lists", lists);
 
     const userId = match.params.userId;
 
-    const handleTEST = e => {
-        // const list = {
-        //     name: "test name",
-        //     user_id: userId
-        // }
-        // createList(list);
-        // fetchLists(userId);
-    }
+    useEffect(() => {
+        if(Object.keys(lists).length === 0) {
+            fetchLists(userId);
+        }
+    }, [lists]);
+
 
     return (
         <section id="lists-page">
             <div>
                 <section>
                     <h2>Lists</h2>
-                    <button onClick={handleTEST}>New List</button>
+                    <button>New List</button>
                 </section>
                 <section>
                     <ListCard/>
