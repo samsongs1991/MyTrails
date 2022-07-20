62971c49e61d61d40f3d091853d6a9896504c61c
Samuel Song <samsongs@umich.edu>
2022-07-20 12:59:53 -0500

    Remove list modal

--- frontend/components/lists/list.jsx
+++ frontend/components/lists/list.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { connect } from "react-redux";
 
 // NOTES
-// copy over what i ened to from edit_profile comp
+// copy over what i need to from edit_profile comp
 // working to have list comp modal have the following
 // - edit photo
 // - edit title
@@ -16,8 +16,8 @@ const List = ({ list, setShowModal }) => {
 
     useEffect(() => {
         const posY = window.pageYOffset;
-        const list = document.getElementById(`list-${list.id}`);
-        list.style.top = `${posY}px`;
+        const modal = document.getElementById(`list-${list.id}`);
+        modal.style.top = `${posY}px`;
         const body = document.querySelector("body");
         body.style.overflow = "hidden";
     }, []);
@@ -40,7 +40,7 @@ const List = ({ list, setShowModal }) => {
     };
 
     return (
-        <section onClick={handleCloseModal} id={`list-${list.id}`}>
+        <section onClick={handleCloseModal} id={`list-${list.id}`} className="list-modal">
             <form onClick={e => e.stopPropagation()} onSubmit={handleSubmit}>
                 <section>
                     <img onClick={handleCloseModal} src="/trail_images/x_icon.png" alt="Close"/>
--- frontend/components/lists/list_card.jsx
+++ frontend/components/lists/list_card.jsx
@@ -1,16 +1,12 @@
 import React, { useState } from "react";
-import List from "./list.jsx";
 
 const ListCard = ({ list }) => {
-    const [showModal, setShowModal] = useState(false);
-
     return (
         <article>
             <img src={list.img_url ? list.img_url : "./list_images/alltrails_symbol.png"} alt="Photo for list"/>
             <div>
                 <p>{list.name}</p>
             </div>
-            {showModal ? <List list={list} setShowModal={setShowModal}/> : null}
         </article>
     );
 };
