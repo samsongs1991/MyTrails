2981ec231b6e99a7f3d738fe0e9fea1a730edb46
Samuel Song <samsongs@umich.edu>
2022-07-21 01:52:21 -0500

    Remove Favorites list seed from demo user

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,8 +11,6 @@ User.create(email: "demo@gmail.com", password: 123456, fname: "Sam", lname: "Son
     .photo
     .attach(io: File.open("./seed_images/demo_profile.jpg"), filename: "demo_profile.jpg")
 
-List.create(name: "Favorites", user_id: 1)
-
 # 20 trails near Seattle, WA
 trails = [
     # easy difficulty
