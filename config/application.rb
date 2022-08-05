2326cf5fb4d9469743c3349ad78c7a8f0caa7460
Samuel Song <samsongs@umich.edu>
2022-08-05 17:30:28 -0500

    Testing heroku fix - Comment out seed file

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module MyTrails
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
@@ -17,5 +18,3 @@ module MyTrails
     # the framework and any gems in your application.
   end
 end
-
-config.assets.initialize_on_precompile = false
