533c55db6d9a933362cd1d12f4444e56b717c002
Samuel Song <samsongs@umich.edu>
2022-08-06 17:14:03 -0500

    Fix bug - mini map not updating when clicking new trail

--- frontend/components/trail/map.jsx
+++ frontend/components/trail/map.jsx
@@ -1,36 +1,35 @@
 import React, { useState, useEffect } from "react";
 
 const Map = ({ trail }) => {
-
     const [map, setMap] = useState(null);
     const [mapNode, setMapNode] = useState(null);
 
     useEffect(() => {
         const mapOptions = {
-            center: { lat: trail.lat, lng: trail.lng }, 
+            center: { lat: trail.lat, lng: trail.lng },
             zoom: 13
         };
         if(mapNode) {
             const newMap = new google.maps.Map(mapNode, mapOptions)
             new google.maps.Marker({
                 position: {
-                    lat: trail.lat, 
+                    lat: trail.lat,
                     lng: trail.lng
-                }, 
-                map: newMap, 
-                title: trail.name, 
+                },
+                map: newMap,
+                title: trail.name,
             });
             setMap(newMap);
         }
-    }, [mapNode]);
-    
+    }, [mapNode, trail]);
+
     return (
         <section id="map">
             <section>
                 <h2>Map</h2>
             </section>
             <section>
-                <div 
+                <div
                     id="map-container"
                     ref={map => setMapNode(map)}
                 >
