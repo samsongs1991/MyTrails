5a14202fa39d1c547ae0a2e5ef6a4b6b07eb6d71
Samuel Song <samsongs@umich.edu>
2022-08-13 23:51:28 -0500

    Add portfolio site link

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -13,7 +13,7 @@ const Footer = props => {
                 <img onClick={() => window.open("https://github.com/samsongs1991/MyTrails", '_blank')} className="social-link-img" src={images.github} alt="GitHub Icon"/>
                 <img onClick={() => window.open("https://www.linkedin.com/in/samsongs1991/", '_blank')} className="social-link-img" src={images.linkedin} alt="LinkedIn Icon"/>
                 <img onClick={() => window.open("https://angel.co/u/samsongs", '_blank')} className="social-link-img" src={images.angellist} alt="AngelList Icon"/>
-                <img onClick={() => window.open("", '_blank')} className="social-link-img" src={images.portfolio} alt="Portfolio Icon"/>
+                <img onClick={() => window.open("https://www.iamsamsong.com", '_blank')} className="social-link-img" src={images.portfolio} alt="Portfolio Icon"/>
             </section>
         </footer>
     );
